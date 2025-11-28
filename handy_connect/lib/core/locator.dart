7243ddc45d066bae5e71c038facf467a56a2b39b
abdrezakkemal@gmail.com <abdrezakkemal@gmail.com>
2025-11-28 10:47:09 +0200

    feat: use get_In for depedency injection

---
+++ handy_connect/lib/core/locator.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:handy_connect/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:handy_connect/features/auth/domain/repositories/auth_repository.dart';
+import 'package:handy_connect/features/auth/domain/usecases/signin.dart';
+import 'package:handy_connect/features/auth/domain/usecases/signup.dart';
+import 'package:handy_connect/features/auth/presentation/bloc/auth_bloc.dart';
+
+final GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  // Blocs
+  locator.registerFactory(() => AuthBloc(signUp: locator(), signIn: locator()));
+
+  // Use Cases
+  locator.registerLazySingleton(() => SignUp(locator()));
+  locator.registerLazySingleton(() => SignIn(locator()));
+
+  // Repositories
+  locator.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
+}
